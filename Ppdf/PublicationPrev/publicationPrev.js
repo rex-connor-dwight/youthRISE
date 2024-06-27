e32eaee3c1bb8816c36fb6cb8006233c12891669
REX CONNOR DWIGHT <115325581+rex-connor-dwight@users.noreply.github.com>
2024-06-27 10:48:05 +0100

    Add files via upload

---
+++ Ppdf/PublicationPrev/publicationPrev.js
@@ -0,0 +1,170 @@
+function downloadPDF() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/Artvocacy.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'Artvocacy.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFPolicyBrief() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/YPB.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'YPB.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+
+function downloadPDFWAP() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/WAP.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'WAP.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFRelease() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/AONDL.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'AONDL.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFADOI() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/ADOI.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'ADOI.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFHMP() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/HMP.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'HMP.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFPVM() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/PVM.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'PVM.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFrEc() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/REC.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'REC.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFWEB() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/WEB.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'WEB.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
+
+function downloadPDFUGS() {
+    // URL of the PDF file
+    var pdfURL = '/Ppdf/UGS.pdf';
+    
+    // Create an anchor element
+    var link = document.createElement('a');
+    
+    // Set the href attribute to the PDF URL
+    link.href = pdfURL;
+    
+    // Set the download attribute to force download
+    link.download = 'UGS.pdf';
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+}
